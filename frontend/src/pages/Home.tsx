a57461751fa3fb937c8bc774a30ce37e75d04c6d
RubnK <40402492+RubnK@users.noreply.github.com>
2025-05-06 14:49:08 +0200

    Correction des imports Tailwind v4

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
-    return (
-      <div className="p-8 text-center">
-        <h1 className="text-4xl font-bold text-blue-600">Bienvenue sur mon portfolio</h1>
-        <p className="mt-4 text-lg text-gray-700">Je suis développeur & photographe.</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Bienvenue sur mon portfolio
+      </h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Je suis développeur & photographe.
+      </p>
+    </div>
+  );
+}
